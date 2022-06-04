7f7e1b85fe82c69777dc4a5e7281f10c31312432
TaylanCann <taylancanh@gmail.com>
2022-06-04 15:08:28 +0300

    Added Connection String

--- Dapper/Program.cs
+++ Dapper/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Dapper
 {
@@ -6,7 +8,28 @@ namespace Dapper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string connString = @"Data Source=LAPTOP-9VG06RAO;Initial Catalog=DapperExampleDb;Integrated Security=True";
+
+            SqlConnection conn = new SqlConnection(connString);
+
+            string sql = "";
+
+            void CRUD()
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+                conn.Execute(sql);
+                conn.Close();
+            }
+
         }
+
+        
+
     }
+
+    
+
 }
